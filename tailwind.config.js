36ee6327ab8a5978b9297d24e825d2bce1880d74
Marcel Schmidt <marcelschmidt1989@live.de>
2025-01-28 15:37:19 +0100

    extend Tailwind CSS theme with custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,39 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: {
+          500: "#98908B",
+          100: "#F8F4F0"
+        },
+        grey: {
+          900: "#201F24",
+          500: "#696868",
+          300: "#B3B3B3",
+          100: "#F2F2F2"
+        },
+        secondary: {
+          green: "#277C78",
+          yellow: "#F2CDAC",
+          cyan: "#82C9D7",
+          navy: "#626070",
+          red: "#C94736",
+          purple: "#826CB0"
+        },
+        other: {
+          purple: "#AF81BA",
+          turquoise: "#597C7C",
+          brown: "#93674F",
+          magenta: "#934F6F",
+          blue: "#3F82B2",
+          grey: "#97A0AC",
+          green: "#7F9161",
+          gold: "#CAB361",
+          orange: "#BE6C49"
+        },
+      }
+    },
   },
   plugins: [],
 }
