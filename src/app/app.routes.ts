87959d5733436c7eb97060a03faaa651af9309c9
Richard Streu <rstreu13@icloud.com>
2025-02-03 12:28:47 +0100

    add routing for home page components: overview, transactions, budgets, pots, and recurring bills

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,26 @@
 import { Routes } from '@angular/router';
+
+// Main pages components
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ImprintComponent } from './pages/imprint/imprint.component';
+
+// Home page components
+import { OverviewComponent } from './pages/home/content/overview/overview.component';
+import { TransactionsComponent } from './pages/home/content/transactions/transactions.component';
+import { BudgetsComponent } from './pages/home/content/budgets/budgets.component';
+import { PotsComponent } from './pages/home/content/pots/pots.component';
+import { RecurringBillsComponent } from './pages/home/content/recurring-bills/recurring-bills.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'homeComponent', component: HomeComponent,
     children: [
-      {path: ''}
+      {path: 'overviewComponent', component: OverviewComponent},
+      {path: 'transactionsComponent', component: TransactionsComponent},
+      {path: 'budgetsComponent', component: BudgetsComponent},
+      {path: 'potsComponent', component: PotsComponent},
+      {path: 'recurringBillsComponent', component: RecurringBillsComponent}
     ]
    },
   { path: 'imprintComponent', component: ImprintComponent },
