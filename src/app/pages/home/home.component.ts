1e92baa15b803668b2a241171978816b2182152a
Richard Streu <rstreu13@icloud.com>
2025-02-03 12:07:01 +0100

    add navbar and content components to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ NavbarComponent, ContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
