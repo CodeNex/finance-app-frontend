19cf9f71210d9986c99dc1411895b83d20b375a5
Richard Streu <rstreu13@icloud.com>
2025-02-04 18:49:59 +0100

    refactor navbar component to use new basedata structure and update styles

--- src/app/pages/home/navbar/navbar.component.ts
+++ src/app/pages/home/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import jsonData from '../../../shared/data/financeapp.basedata.json';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ ],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  public navData = jsonData.financeapp.navbar.links;
+}
