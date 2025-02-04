d76ee9f1fe1f78def0cf7e8adb81e7193e6de4d8
Richard Streu <rstreu13@icloud.com>
2025-02-04 13:40:06 +0100

    add Navlink component to navbar with basic template and test setup

--- src/app/pages/home/navbar/navbar.component.ts
+++ src/app/pages/home/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NavlinkComponent } from './navlink/navlink.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ NavlinkComponent ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
---
+++ src/app/pages/home/navbar/navlink/navlink.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navlink',
+  imports: [],
+  templateUrl: './navlink.component.html',
+  styleUrl: './navlink.component.scss'
+})
+export class NavlinkComponent {
+
+}
