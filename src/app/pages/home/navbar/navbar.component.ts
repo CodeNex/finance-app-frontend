516b3553f4f04f874c71f659dab1e9bcbe514dd6
Richard Streu <rstreu13@icloud.com>
2025-02-05 16:53:39 +0100

    refactor Navbar to improve slide button functionality and add transition effects

--- src/app/pages/home/navbar/navbar.component.ts
+++ src/app/pages/home/navbar/navbar.component.ts
@@ -19,16 +19,18 @@ export class NavbarComponent {
       document.querySelectorAll('[data-group="navLinkName"]')
     );
     let navBarRef = document.getElementById('navBar');
+    let slideButtonRef = document.getElementById('slideButton');
     if (!this.isNavbarThin) {
-      this.makeNavbarThin(navLinkNames, navBarRef);
+      this.makeNavbarThin(navLinkNames, navBarRef, slideButtonRef);
     } else {
-      this.makeNavbarWide(navLinkNames, navBarRef);
+      this.makeNavbarWide(navLinkNames, navBarRef, slideButtonRef);
     }
     this.isNavbarThin = !this.isNavbarThin;
     console.log('isNavbarThin', this.isNavbarThin);
   }
 
-  makeNavbarThin(navLinkNames: Array<any>, navBarRef: HTMLElement | null) {
+  makeNavbarThin(navLinkNames: Array<any>, navBarRef: HTMLElement | null, slideButtonRef: HTMLElement | null) {
+    if (slideButtonRef) slideButtonRef.classList.add("slideButtonReturn");
     navLinkNames.forEach((navLinkName) =>
       navLinkName.classList.add('opacity_zero')
     );
@@ -41,9 +43,10 @@ export class NavbarComponent {
     }, 300);
   }
 
-  makeNavbarWide(navLinkNames: Array<any>, navBarRef: HTMLElement | null) {
+  makeNavbarWide(navLinkNames: Array<any>, navBarRef: HTMLElement | null, slideButtonRef: HTMLElement | null) {
     navLinkNames.forEach((link) => link.classList.remove('d_none'));
     setTimeout(() => {
+      if (slideButtonRef) slideButtonRef.classList.remove("slideButtonReturn");
       navLinkNames.forEach((link) => link.classList.remove('opacity_zero'));
       if (navBarRef) {
         navBarRef?.classList.remove('navbar-thin');
