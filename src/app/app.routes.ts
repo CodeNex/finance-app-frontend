2ea52dc29bae2b3aaaafb3025ce1a42f663c746d
Richard Streu <rstreu13@icloud.com>
2025-02-05 08:16:35 +0100

    update routing to set login path and redirect home to overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,10 +15,10 @@ import { RecurringBillsComponent } from './pages/home/content/recurring-bills/re
 import { SettingsComponent } from './pages/home/content/settings/settings.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'registrate', component: RegistrateComponent, pathMatch: 'full' },
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
     children: [
       { path: '', redirectTo: 'overview', pathMatch: 'full' },
