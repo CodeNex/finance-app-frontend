0b189a3317426eca864085e3e9c105eca537a046
Richard Streu <rstreu13@icloud.com>
2025-02-05 12:22:55 +0100

    add icon rendering logic to IconsComponent and integrate it into Navbar

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component, Input, } from '@angular/core';
 
 @Component({
   selector: 'app-icons',
@@ -7,5 +7,5 @@ import { Component, } from '@angular/core';
   styleUrl: './icons.component.scss'
 })
 export class IconsComponent {
-  
+  @Input("iconName") public readonly iconName: any = "";
 }
--- src/app/pages/home/navbar/navbar.component.ts
+++ src/app/pages/home/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import jsonData from '../../../shared/data/financeapp.basedata.json';
+import { IconsComponent } from '../../../components/icons/icons.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, IconsComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
