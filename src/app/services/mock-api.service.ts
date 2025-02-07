0b9496536fe86e427d304b8939d31454f8ef5b80
Richard Streu <rstreu13@icloud.com>
2025-02-07 15:32:58 +0100

    feat: enhance MockApiService with caching and CRUD operations for balance, budgets, pots, and transactions

--- src/app/services/mock-api.service.ts
+++ src/app/services/mock-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs';
 import { DataStoreServiceService } from './data-store-service.service';
 
@@ -17,21 +17,62 @@ export class MockApiService {
 
   // GET: call Balance 
   getBalance(): Observable<any> {
-    return this.http.get(`${this.baseUrl}balance.json`);
+    if (this.dataStore.getBalance()) {
+      return of (this.dataStore.getBalance()
+      )
+    }
+    return this.http.get(`${this.baseUrl}balance.json`).pipe(
+      tap((data: any) => this.dataStore.setBalance(data))
+    );
+  }
+
+  // Update: PUT Balance
+  updateBalance(newBalance: any): Observable<any> {
+    this.dataStore.setBalance(newBalance);
+    return of(newBalance);
   }
 
   // GET: call Budgets
-  getBudgets(): Observable<any> {
-    return this.http.get(`${this.baseUrl}budgets.json`);
+  getBudgets(): Observable<any[]> {
+    if (this.dataStore.getBudgets().length > 0) {
+      return of(this.dataStore.getBudgets());
+    }
+    return this.http.get<any[]>(`${this.baseUrl}budgets.json`).pipe(
+      tap(data => this.dataStore.setBudgets(data))
+    );
   }
 
-  // GET: call Pots
-  getPots(): Observable<any> {
-    return this.http.get(`${this.baseUrl}pots.json`);
+  // POST: add New Budgets
+  addBudget(newBudget: any): Observable<any> {
+    const budgets = [...this.dataStore.getBudgets(), newBudget];
+    this.dataStore.setBudgets(budgets);
+    return of(newBudget);
   }
 
-  // GET: call Transactions
-  getTransactions(): Observable<any> {
-    return this.http.get(`${this.baseUrl}transactions.json`);
+  // DELETE: delete Budgets
+  deleteBudget(id: number): Observable<any> {
+    const budgets = this.dataStore.getBudgets().filter(b => b.id !== id);
+    this.dataStore.setBudgets(budgets);
+    return of({ success: true });
+  }
+
+   // GET: Pots abrufen
+   getPots(): Observable<any[]> {
+    if (this.dataStore.getPots().length > 0) {
+      return of(this.dataStore.getPots());
+    }
+    return this.http.get<any[]>(`${this.baseUrl}pots.json`).pipe(
+      tap(data => this.dataStore.setPots(data))
+    );
+  }
+
+  // GET: Transactions abrufen
+  getTransactions(): Observable<any[]> {
+    if (this.dataStore.getTransactions().length > 0) {
+      return of(this.dataStore.getTransactions());
+    }
+    return this.http.get<any[]>(`${this.baseUrl}transactions.json`).pipe(
+      tap(data => this.dataStore.setTransactions(data))
+    );
   }
 }
