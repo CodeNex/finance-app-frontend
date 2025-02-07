655b17116cea8bf6e2eb0771efa0fd46d31cb46b
Richard Streu <rstreu13@icloud.com>
2025-02-07 15:10:16 +0100

    feat: refactor MockApiService to use Angular's dependency injection for HttpClient and DataStoreService

--- src/app/services/mock-api.service.ts
+++ src/app/services/mock-api.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs';
+import { DataStoreServiceService } from './data-store-service.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,10 @@ import { Observable } from 'rxjs';
 export class MockApiService {
 
   private baseUrl = "/public/dummyData/";
+  private http: HttpClient = inject(HttpClient);
+  private dataStore: DataStoreServiceService = inject(DataStoreServiceService);
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   // GET: call Balance 
   getBalance(): Observable<any> {
