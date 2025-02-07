f3b423bfb642898414c85bcfe4c63736588d5a83
Richard Streu <rstreu13@icloud.com>
2025-02-07 14:54:20 +0100

    feat: add getter and setter methods for balance, budgets, pots, and transactions in DataStoreService

--- src/app/services/data-store-service.service.ts
+++ src/app/services/data-store-service.service.ts
@@ -18,4 +18,36 @@ export class DataStoreServiceService {
   public transactions: TransactionsObject[] = [];
 
   constructor() {}
+
+  // save and call Balance
+  setBalance(data: BalanceObject) {
+    this.balance = data;
+  }
+  getBalance() {
+    return this.balance;
+  }
+
+  // save and call Budgets
+  setBudgets(data: BudgetsObject[]) {
+    this.budgets = data;
+  }
+  getBudgets() {
+    return this.budgets;
+  }
+
+  // save and call Pots
+  setPots(data: PotsObject[]) {
+    this.pots = data;
+  }
+  getPots() {
+    return this.pots;
+  }
+
+  // save and call Transactions
+  setTransactions(data: TransactionsObject[]) {
+    this.transactions = data;
+  }
+  getTransactions() {
+    return this.transactions;
+  }
 }
