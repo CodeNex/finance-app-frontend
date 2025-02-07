223b155f9f129c8d7e092f9b2b903e99d642a24d
Richard Streu <rstreu13@icloud.com>
2025-02-07 13:47:01 +0100

    feat: enhance MockApiService with HTTP client methods for fetching balance, budgets, pots, and transactions

--- src/app/services/mock-api.service.ts
+++ src/app/services/mock-api.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MockApiService {
 
-  constructor() { }
+  private baseUrl = "/public/dummyData/";
+
+  constructor(private http: HttpClient) { }
+
+  // GET: call Balance 
+  getBalance(): Observable<any> {
+    return this.http.get(`${this.baseUrl}balance.json`);
+  }
+
+  // GET: call Budgets
+  getBudgets(): Observable<any> {
+    return this.http.get(`${this.baseUrl}budgets.json`);
+  }
+
+  // GET: call Pots
+  getPots(): Observable<any> {
+    return this.http.get(`${this.baseUrl}pots.json`);
+  }
+
+  // GET: call Transactions
+  getTransactions(): Observable<any> {
+    return this.http.get(`${this.baseUrl}transactions.json`);
+  }
 }
