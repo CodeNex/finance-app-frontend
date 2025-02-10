1d49e353e5ca43eca4c62079dc0569dec325f058
Richard Streu <rstreu13@icloud.com>
2025-02-10 10:25:24 +0100

    feat: add getData method to APIService for fetching data with custom headers

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of, tap } from 'rxjs';
 import { LoginService } from './login.service';
 import { DataStoreServiceService } from './data-store-service.service';
@@ -15,4 +15,16 @@ export class APIService {
   private dataStore: DataStoreServiceService = inject(DataStoreServiceService);
 
   constructor() { }
+
+  // get data from the server
+  // endpoints: balance, budgets, pots, transactions
+  getData(endpoint: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer dein-token-hier',
+      'Custom-Header': 'value'
+    });
+
+    return this.http.get(`${this.baseUrl}/${endpoint}`, { headers });
+  }
+
 }
