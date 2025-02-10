934b1d863a91a99cb7b30ece69a67e2c9b4ca784
Richard Streu <rstreu13@icloud.com>
2025-02-10 10:19:54 +0100

    feat: replace MockApiService with APIService and add initial implementation and tests

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
+import { LoginService } from './login.service';
 import { DataStoreServiceService } from './data-store-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MockApiService {
+export class APIService {
 
   private baseUrl = "/public/dummyData/";
   private http: HttpClient = inject(HttpClient);
+  private logIn: LoginService = inject(LoginService);
   private dataStore: DataStoreServiceService = inject(DataStoreServiceService);
 
   constructor() { }
-
-
 }
