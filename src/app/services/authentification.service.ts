689e67558e623f7e2dc211006594db1b5bcfd9ea
Richard Streu <rstreu13@icloud.com>
2025-02-11 13:24:29 +0100

    feat: improve error handling in authentication service and log error details

--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -51,7 +51,9 @@ export class AuthentificationService {
         error: (error) => {
           if (authOption === 'login' || 'guest') this.warningScreenLogin = true;
           if (authOption === 'register') this.warningScreenRegister = true;
-          this.authWarningMessage = error.error.message;
+          this.authWarningMessage = error.message;
+          console.log('Error:', error);
+          
         },
       });
   }
