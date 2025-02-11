dd2db1e25530d7d3ec77157eb420983de99be498
Richard Streu <rstreu13@icloud.com>
2025-02-11 10:11:08 +0100

    feat: add guest login button and update login form component

--- src/app/pages/login/login-form/login-form.component.ts
+++ src/app/pages/login/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IconsComponent } from '../../../components/icons/icons.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-form',
@@ -9,17 +10,23 @@ import { IconsComponent } from '../../../components/icons/icons.component';
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+
+  baseUrl: string = 'http://localhost:3000';
+
+  /**
+   * Emits an event to change the window between login and register components
+   */
   @Output() changeWindow = new EventEmitter();
 
   emitChangeWindow(windowName: string) {
     this.changeWindow.emit(windowName);
   }
 
-  isPasswordVisible: boolean = false;
-
   /**
    * Toggles the visibility of the password input field
    */
+  isPasswordVisible: boolean = false;
+
   changePasswordVisibility() {
     this.isPasswordVisible = !this.isPasswordVisible;
     let passwordInputRef = document.getElementById(
@@ -27,5 +34,9 @@ export class LoginFormComponent {
     ) as HTMLInputElement;
     passwordInputRef.type =
       passwordInputRef.type === 'password' ? 'text' : 'password';
+  }
+
+  doGuestLogin() {
+
   }
 }
