e3a1adcf37803f4166b4c2f9fdf670a7e868b478
Richard Streu <rstreu13@icloud.com>
2025-02-17 18:16:29 +0100

    feat: enhance imprint component with back navigation button and icon for improved user experience

--- src/app/pages/imprint/imprint.component.ts
+++ src/app/pages/imprint/imprint.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
+import { IconsComponent } from '../../components/icons/icons.component';
+
 @Component({
   selector: 'app-imprint',
-  imports: [RouterModule],
+  imports: [RouterModule, IconsComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
