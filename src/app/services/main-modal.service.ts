19bf7ac3c2b69db65910d6937436b40e8c62a274
Richard Streu <rstreu13@icloud.com>
2025-02-20 11:50:03 +0100

    feat: add show and hide methods to MainModalService for managing modal visibility

--- src/app/services/main-modal.service.ts
+++ src/app/services/main-modal.service.ts
@@ -8,4 +8,12 @@ export class MainModalService {
   constructor() { }
 
   public isMainModalVisible: boolean = false;
+
+  public showMainModal() {
+    this.isMainModalVisible = true;
+  }
+
+  public hideMainModal() {
+    this.isMainModalVisible = false;
+  }
 }
