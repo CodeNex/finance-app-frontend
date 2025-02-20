41fb2d3b03091240d955db61e7a14010ca8f8a27
Richard Streu <rstreu13@icloud.com>
2025-02-20 17:55:23 +0100

    feat: initialize main modal visibility and current sub modal state

--- src/app/services/main-modal.service.ts
+++ src/app/services/main-modal.service.ts
@@ -9,7 +9,7 @@ export class MainModalService {
   constructor() { }
 
   // show/hide main modal
-  @Output() public isMainModalVisible$ = new BehaviorSubject<boolean>(true);
+  @Output() public isMainModalVisible$ = new BehaviorSubject<boolean>(false);
 
   public showMainModal() {
     this.isMainModalVisible$.next(true);
@@ -22,7 +22,7 @@ export class MainModalService {
   // choose current sub modal
   // this will be used to determine which sub modal to show
   // Types of sub modal: addBudget, deleteBudget, editBudget, addPot, editPot, deletePot, addMoneyPot, withdrawMoneyPot
-  @Output() public currentSubModal$ = new BehaviorSubject<string>('withdrawMoneyPot');
+  @Output() public currentSubModal$ = new BehaviorSubject<string>('');
   @Output() public subModalObject$ = new BehaviorSubject<Object>({});
 
   public chooseSubModal(subModal: string, subModalObject: Object) {
