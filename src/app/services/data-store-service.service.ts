6f3ad553d758e451f882a8378ac635f11348200e
Richard Streu <rstreu13@icloud.com>
2025-02-21 12:34:38 +0100

    feat: refactor DataStoreService to use signals for pots and transactions

--- src/app/services/data-store-service.service.ts
+++ src/app/services/data-store-service.service.ts
@@ -5,61 +5,63 @@ import '../shared/interfaces.ts';
   providedIn: 'root',
 })
 export class DataStoreServiceService {
+
   public balance = signal<BalanceObject>({
-    "id": 1,
-    "current": 0.00,
-    "income": 3250.00,
-    "expenses": 2188.00
+    id: 1,
+    current: 0.0,
+    income: 3250.0,
+    expenses: 2188.0,
   });
 
   public budgets = signal<BudgetsObject[]>([]);
 
-  // public pots: PotsObject[] = [];
-  public transactions: TransactionsObject[] = [];
-  public transactionsRecurring: TransactionsObject[] = [];
+  public pots = signal<PotsObject[]>([]);
+  // public pots = signal<PotsObject[]>([
+  //   {
+  //     id: 0,
+  //     name: 'Savings',
+  //     target: 2000.0,
+  //     total: 159.0,
+  //     theme: '#277C78',
+  //     deletedAt: null,
+  //   },
+  //   {
+  //     id: 1,
+  //     name: 'Concert Ticket',
+  //     target: 150.0,
+  //     total: 110.0,
+  //     theme: '#626070',
+  //     deletedAt: null,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Gift',
+  //     target: 150.0,
+  //     total: 110.0,
+  //     theme: '#82C9D7',
+  //     deletedAt: null,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'New Laptop',
+  //     target: 1000.0,
+  //     total: 10.0,
+  //     theme: '#F2CDAC',
+  //     deletedAt: null,
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Holiday',
+  //     target: 1440.0,
+  //     total: 531.0,
+  //     theme: '#826CB0',
+  //     deletedAt: null,
+  //   },
+  // ]);
 
-  public pots: PotsObject[] = [
-    {
-      id: 0,
-      name: 'Savings',
-      target: 2000.0,
-      total: 159.0,
-      theme: '#277C78',
-      deletedAt: null,
-    },
-    {
-      id: 1,
-      name: 'Concert Ticket',
-      target: 150.0,
-      total: 110.0,
-      theme: '#626070',
-      deletedAt: null,
-    },
-    {
-      id: 2,
-      name: 'Gift',
-      target: 150.0,
-      total: 110.0,
-      theme: '#82C9D7',
-      deletedAt: null,
-    },
-    {
-      id: 3,
-      name: 'New Laptop',
-      target: 1000.0,
-      total: 10.0,
-      theme: '#F2CDAC',
-      deletedAt: null,
-    },
-    {
-      id: 4,
-      name: 'Holiday',
-      target: 1440.0,
-      total: 531.0,
-      theme: '#826CB0',
-      deletedAt: null,
-    },
-  ];
+  public transactions = signal<TransactionsObject[]>([]);
+
+  public transactionsRecurring = signal<TransactionsObject[]>([]);
 
   constructor() {}
 
