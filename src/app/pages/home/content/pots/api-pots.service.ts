abb7914845956b10d5d8b252e4d3647c081fb764
Richard Streu <rstreu13@icloud.com>
2025-02-26 14:06:26 +0100

    feat: implement addNewPot method in ApiPotsService for creating new pots

--- src/app/pages/home/content/pots/api-pots.service.ts
+++ src/app/pages/home/content/pots/api-pots.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+
+import { AuthenticationService } from '../../../../services/authentication.service';
+import { BasedataService } from '../../../../services/basedata.service';
+import { DataStoreServiceService } from '../../../../services/data-store-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiPotsService {
 
+  private baseData: BasedataService = inject(BasedataService);
+  private http: HttpClient = inject(HttpClient);
+  private AuthenticationService: AuthenticationService = inject(
+    AuthenticationService
+  );
+  private dataStore: DataStoreServiceService = inject(DataStoreServiceService);
+
+  private baseUrl: string = this.baseData.financeApp.basics.apiData.baseUrl;
+
+  public warningMessage: string = '';
+
   constructor() { }
+
+  // function to add new pots
+  // response: {message: "Pot created"}
+
+  addNewPot(potObject: any){
+    const path = 'pots';
+    const body = potObject;
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.AuthenticationService.authToken}`,
+      Accept: 'application/json',
+    });
+
+    this.http.post(`${this.baseUrl}/${path}`, body, { headers}).subscribe({
+      next: (response) => {},
+      error: (error) => {}
+    })
+    
+  }
+
+  // function to update existing specific pot
+  // response: {message: "Pot updated"}
+
+  // function to delete specific pot
+  // response: {message: "Pot deleted"} 
 }
