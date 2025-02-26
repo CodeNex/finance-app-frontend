814a6017c683e9f781feb888daedc0f55758646b
Richard Streu <rstreu13@icloud.com>
2025-02-26 08:42:34 +0100

    refactor: update progress bar gradient and set default amount percentage for improved visual feedback

--- src/app/pages/home/content/pots/pots-modals/withdrawmoney-pot-modal/withdrawmoney-pot-modal.component.ts
+++ src/app/pages/home/content/pots/pots-modals/withdrawmoney-pot-modal/withdrawmoney-pot-modal.component.ts
@@ -40,7 +40,7 @@ export class WithdrawmoneyPotModalComponent {
   public progressBarPercentage: string = '';
 
   public prevPercentageBar: string = '';
-  public amountPercentageBar: number = 0;
+  public amountPercentageBar: number = 100;
 
   public inputValue: number | null = null;
 
