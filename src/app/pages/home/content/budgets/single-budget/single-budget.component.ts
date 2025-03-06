73bd087ac2b69ed15030e1c7c44394d965509f68
Richard Streu <rstreu13@icloud.com>
2025-03-06 11:27:19 +0100

    feat: implement budget pop-up functionality with open and close methods

--- src/app/pages/home/content/budgets/single-budget/single-budget.component.ts
+++ src/app/pages/home/content/budgets/single-budget/single-budget.component.ts
@@ -56,5 +56,60 @@ export class SingleBudgetComponent {
 
   @Input() public budgetIndex: number = -1;
 
-  ngOnInit() {}
+  public isPopUpOpen: boolean = false;
+
+  public maximum: string = '';
+  public spent: string = '';
+  public remaining: string = '';
+
+  ngOnInit() {
+    this.maximum = `$${this.budget.maximum.toLocaleString('en-US', {
+      maximumFractionDigits: 0,
+    })}`;
+    this.spent = `$${this.budget.amount.toLocaleString('en-US', {
+      maximumFractionDigits: 0,
+    })}`;
+    this.remaining = `$${(
+      this.budget.maximum - this.budget.amount
+    ).toLocaleString('en-US', {
+      maximumFractionDigits: 0,
+    })}`;
+  }
+
+  // Open the modal when the user clicks on any button which opens a modal, givs the modal name as a string and the current pot object as "subModalObject" to the function as arguments
+  public openSubModal(subModal: string, subModalObject: Object) {
+    this.mainModalService.chooseSubModal(
+      subModal,
+      subModalObject,
+      this.budgetIndex
+    );
+    this.isPopUpOpen = false;
+  }
+
+  // Open the pop-up when the user clicks on the three dots
+  public openPopUp() {
+    if (this.isPopUpOpen) return;
+    setTimeout(() => {
+      this.isPopUpOpen = true;
+      document.addEventListener('click', this.closePopUp.bind(this));
+      console.log(
+        'Budget with index ' + this.budgetIndex + ' is open: ' + this.isPopUpOpen
+      );
+    }, 20);
+    return;
+  }
+
+  // Close the pop-up if the user clicks outside of the pop-up
+  public closePopUp(event: MouseEvent) {
+    if (!this.isPopUpOpen) return;
+    let target = event.target as HTMLElement;
+    if (!target) return;
+    let allowedIDs = ['editPotButton', 'deletePotButton', 'potPopUp'];
+    if (allowedIDs.includes(target.id)) return;
+    this.isPopUpOpen = false;
+    document.removeEventListener('click', this.closePopUp.bind(this));
+    console.log(
+      'Budget with index ' + this.budgetIndex + ' is open: ' + this.isPopUpOpen
+    );
+  }
 }
