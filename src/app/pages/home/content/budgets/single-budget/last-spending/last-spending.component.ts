20f540018c6dbd652aea4edd45057bcd9056c379
Richard Streu <rstreu13@icloud.com>
2025-03-06 11:36:59 +0100

    feat: add LastSpending component with template, styles, and tests

---
+++ src/app/pages/home/content/budgets/single-budget/last-spending/last-spending.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-last-spending',
+  imports: [],
+  templateUrl: './last-spending.component.html',
+  styleUrl: './last-spending.component.scss'
+})
+export class LastSpendingComponent {
+
+}
