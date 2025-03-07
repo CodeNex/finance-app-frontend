0ea2f7fdb5c77dd3bdacf73586423de166c9701c
Richard Streu <rstreu13@icloud.com>
2025-03-07 08:03:15 +0100

    feat: integrate last spending component with input binding for dynamic data display

--- src/app/pages/home/content/budgets/single-budget/last-spending/last-spending.component.ts
+++ src/app/pages/home/content/budgets/single-budget/last-spending/last-spending.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-last-spending',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './last-spending.component.html',
-  styleUrl: './last-spending.component.scss'
+  styleUrl: './last-spending.component.scss',
 })
 export class LastSpendingComponent {
-
+  @Input() public spending: any = {
+    id: 1,
+    user: 1,
+    name: 'Initial Deposit',
+    amount: 100.0,
+    recurring: null,
+    budget_id: null,
+    deleted_at: null,
+    created_at: '2025-02-24T16:14:01.000000Z',
+    category: '',
+    budget: {
+      name: 'Entertainment',
+    },
+  };
 }
