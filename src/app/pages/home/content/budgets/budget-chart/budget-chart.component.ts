2825a1f8077892482c56bbf0ed4d839651430171
Richard Streu <rstreu13@icloud.com>
2025-03-10 09:09:13 +0100

    feat: integrate ng2-charts into budget chart component for enhanced chart functionality

--- src/app/pages/home/content/budgets/budget-chart/budget-chart.component.ts
+++ src/app/pages/home/content/budgets/budget-chart/budget-chart.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-budget-chart',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './budget-chart.component.html',
   styleUrl: './budget-chart.component.scss'
 })
