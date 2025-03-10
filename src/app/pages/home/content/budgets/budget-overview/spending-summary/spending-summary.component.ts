714f2376357de132304dbf3e4cd674b33f9ec678
Richard Streu <rstreu13@icloud.com>
2025-03-10 19:57:55 +0100

    refactor: replace placeholder with spending summary item component in spending summary

--- src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary.component.ts
+++ src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary.component.ts
@@ -2,17 +2,16 @@ import { Component, inject } from '@angular/core';
 
 import { DataStoreServiceService } from '../../../../../../services/data-store-service.service';
 import { CommonModule } from '@angular/common';
+import { SpendingSummaryItemComponent } from './spending-summary-item/spending-summary-item.component';
 
 @Component({
   selector: 'app-spending-summary',
-  imports: [CommonModule],
+  imports: [CommonModule, SpendingSummaryItemComponent],
   templateUrl: './spending-summary.component.html',
-  styleUrl: './spending-summary.component.scss'
+  styleUrl: './spending-summary.component.scss',
 })
 export class SpendingSummaryComponent {
-
   public dataStore: DataStoreServiceService = inject(DataStoreServiceService);
 
   public budgetsArray = this.dataStore.budgets();
-
 }
