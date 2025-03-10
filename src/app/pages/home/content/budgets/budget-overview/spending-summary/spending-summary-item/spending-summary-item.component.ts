79a14e425deee3c99cd8cdfcbb99a691595a1c43
Richard Streu <rstreu13@icloud.com>
2025-03-10 20:01:43 +0100

    feat: add default summary item structure and log output in spending summary item component

--- src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary-item/spending-summary-item.component.ts
+++ src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary-item/spending-summary-item.component.ts
@@ -5,9 +5,21 @@ import { Component, inject, Input } from '@angular/core';
   selector: 'app-spending-summary-item',
   imports: [CommonModule],
   templateUrl: './spending-summary-item.component.html',
-  styleUrl: './spending-summary-item.component.scss'
+  styleUrl: './spending-summary-item.component.scss',
 })
 export class SpendingSummaryItemComponent {
+  @Input() public summaryItem: any = {
+    amount: 0,
+    created_at: null,
+    deleted_at: null,
+    id: -11,
+    last_spendings: [],
+    maximum: -1,
+    name: 'Example',
+    theme: '#93674F',
+  };
 
-  @Input() public summaryItem: any;
+  ngOnInit() {
+    console.log(this.summaryItem);
+  }
 }
