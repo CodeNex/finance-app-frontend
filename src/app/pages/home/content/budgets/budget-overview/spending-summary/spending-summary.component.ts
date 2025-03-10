f1af0e88fd3bd5163701f9e4b73d5f2ce3a4ba21
Richard Streu <rstreu13@icloud.com>
2025-03-10 19:29:09 +0100

    refactor: enhance spending summary component layout and integrate budgets data

--- src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary.component.ts
+++ src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary.component.ts
@@ -1,15 +1,18 @@
 import { Component, inject } from '@angular/core';
 
 import { DataStoreServiceService } from '../../../../../../services/data-store-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-spending-summary',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spending-summary.component.html',
   styleUrl: './spending-summary.component.scss'
 })
 export class SpendingSummaryComponent {
 
   public dataStore: DataStoreServiceService = inject(DataStoreServiceService);
+
+  public budgetsArray = this.dataStore.budgets();
 
 }
