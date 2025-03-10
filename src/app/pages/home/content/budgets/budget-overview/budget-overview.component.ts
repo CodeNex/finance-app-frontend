0857497b2acedc08199546a1a3d50c261bfc758c
Richard Streu <rstreu13@icloud.com>
2025-03-10 08:57:17 +0100

    feat: add budget chart and overview components with basic structure and tests

---
+++ src/app/pages/home/content/budgets/budget-overview/budget-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-overview',
+  imports: [],
+  templateUrl: './budget-overview.component.html',
+  styleUrl: './budget-overview.component.scss'
+})
+export class BudgetOverviewComponent {
+
+}
