62a8a113a0eba310a0bbab474a693ff92d2414d1
Richard Streu <rstreu13@icloud.com>
2025-03-18 09:00:46 +0100

    feat: update pagination-transactions component to emit changePage event and log currentPage after delay

--- src/app/pages/home/content/transactions/pagination-transactions/pagination-transactions.component.ts
+++ src/app/pages/home/content/transactions/pagination-transactions/pagination-transactions.component.ts
@@ -1,24 +1,34 @@
-import { Component, Input, Signal, computed, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Signal,
+  computed,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pagination-transactions',
   imports: [],
   templateUrl: './pagination-transactions.component.html',
-  styleUrl: './pagination-transactions.component.scss'
+  styleUrl: './pagination-transactions.component.scss',
 })
 export class PaginationTransactionsComponent {
-
-  @Input() public currentPage$!: Signal<number>; 
+  @Input() public currentPage$!: Signal<number>;
 
   public currentPage = computed(() => this.currentPage$());
 
-  @Output() public changePage: EventEmitter<number> = new EventEmitter<number>();
+  @Output() public changePage: EventEmitter<number> =
+    new EventEmitter<number>();
 
   public changePageHandler(page: number) {
     this.changePage.emit(page);
   }
 
   ngOnInit() {
-    console.log(this.currentPage());
+    setTimeout(() => {
+      this.changePageHandler(7);
+      console.log(this.currentPage$());
+    }, 3000);
   }
 }
