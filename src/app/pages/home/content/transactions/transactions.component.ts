6df4ccff5827c92e99484c2b3ff9cc546590914e
Richard Streu <rstreu13@icloud.com>
2025-03-18 11:34:02 +0100

    feat: update pagination logic to conditionally render section and increase transactions per page

--- src/app/pages/home/content/transactions/transactions.component.ts
+++ src/app/pages/home/content/transactions/transactions.component.ts
@@ -87,7 +87,7 @@ export class TransactionsComponent {
   }
 
   private splitTransactionsArray(prevArray: any) {
-    let transactionsPerPage = 6;
+    let transactionsPerPage = 7;
     let splittedArray: any[][] = [];
     for (let i = 0; i < prevArray.length; i += transactionsPerPage) {
       splittedArray.push(prevArray.slice(i, i + transactionsPerPage));
