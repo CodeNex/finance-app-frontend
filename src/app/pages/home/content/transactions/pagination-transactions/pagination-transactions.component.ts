c4e3649daaa8d46cffa182cf2e63745b364743fc
Richard Streu <rstreu13@icloud.com>
2025-03-18 08:46:25 +0100

    feat: integrate reactive currentPage signal into pagination-transactions component

--- src/app/pages/home/content/transactions/pagination-transactions/pagination-transactions.component.ts
+++ src/app/pages/home/content/transactions/pagination-transactions/pagination-transactions.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-pagination-transactions',
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PaginationTransactionsComponent {
 
+  @Input() public currentPage$!: Signal<number>; 
+
+  public currentPage = computed(() => this.currentPage$());
+
+  ngOnInit() {
+    console.log(this.currentPage());
+  }
 }
