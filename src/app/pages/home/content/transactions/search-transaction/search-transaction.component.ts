abb21cf38d1637641457b0add8383b9fb9226a54
Richard Streu <rstreu13@icloud.com>
2025-03-19 07:46:13 +0100

    feat: add search field change event emission in search transaction component

--- src/app/pages/home/content/transactions/search-transaction/search-transaction.component.ts
+++ src/app/pages/home/content/transactions/search-transaction/search-transaction.component.ts
@@ -8,4 +8,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class SearchTransactionComponent {
 
+  @Output() public searchFieldChange: EventEmitter<string> = new EventEmitter<string>();
+
+  public emitSearchFieldChange(input: string) {
+    this.searchFieldChange.emit(input);
+  }
+
+  ngOnInit() {}
 }
