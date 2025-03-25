25a3ad99b45ed3ac70c02378c7b248ec6071482f
Richard Streu <rstreu13@icloud.com>
2025-03-25 15:13:37 +0100

    feat: inject DataStoreServiceService into ApiTransactionService for improved data access

--- src/app/pages/home/content/transactions/api-transaction.service.ts
+++ src/app/pages/home/content/transactions/api-transaction.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+
+import { DataStoreServiceService } from '../../../../services/data-store-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiTransactionService {
+
+  public dataStore: DataStoreServiceService = inject(DataStoreServiceService);
 
   constructor() { }
 }
