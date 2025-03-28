19af8546bb4c74291707da709a68972ad042cb8d
Richard Streu <rstreu13@icloud.com>
2025-03-28 08:41:43 +0100

    feat: add transaction handling methods and comments in api-transaction.service

--- src/app/pages/home/content/transactions/api-transaction.service.ts
+++ src/app/pages/home/content/transactions/api-transaction.service.ts
@@ -10,4 +10,22 @@ export class ApiTransactionService {
   public dataStore: DataStoreServiceService = inject(DataStoreServiceService);
 
   constructor() { }
+
+  // was muss welche transaction machen?  
+  
+  // Add New Transaction (Single Transaction) -> Transaction object is received
+
+  // Add New Transaction (Recurring Transaction) -> Transaction object is received
+
+  // Pots Add Money -> have to create a transaction object
+
+  // Pots Withdraw Money -> have to create a transaction object
+
+  // after weh have a every transaction object, we have to do three things:
+
+  // 1. POST the transaction object to the server ---> if Respones is ok, the next two steps will be exceuted
+  // 2. add the transaction object to the dataStore (update transactions)
+
+
+  public startTransaction() {}
 }
