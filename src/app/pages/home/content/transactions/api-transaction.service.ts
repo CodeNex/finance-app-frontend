46fa94977d95a12e1e4182e08e17f9b03970aad6
Richard Streu <rstreu13@icloud.com>
2025-03-28 08:58:25 +0100

    feat: enhance transaction handling with detailed comments and add currentTransaction structure in api-transaction.service

--- src/app/pages/home/content/transactions/api-transaction.service.ts
+++ src/app/pages/home/content/transactions/api-transaction.service.ts
@@ -11,7 +11,7 @@ export class ApiTransactionService {
 
   constructor() { }
 
-  // was muss welche transaction machen?  
+  // what kind of transaction do we have? And what has every transaction to do? 
   
   // Add New Transaction (Single Transaction) -> Transaction object is received
 
@@ -21,11 +21,33 @@ export class ApiTransactionService {
 
   // Pots Withdraw Money -> have to create a transaction object
 
+  //
+
   // after weh have a every transaction object, we have to do three things:
 
   // 1. POST the transaction object to the server ---> if Respones is ok, the next two steps will be exceuted
   // 2. add the transaction object to the dataStore (update transactions)
+  // 3. update the balance signal object in the dataStore
 
 
-  public startTransaction() {}
+  public currentTransaction: any = {
+    transaction_id: 0,
+    user_id: 0,
+    amount: 0,
+    budget_id: null,
+    created_at: '2025-09-12T00:00:00Z',
+    execute_on: '2025-07-26T00:00:00Z',
+    deleted_at: null,
+    recurring: null,
+    recurring_id: null,
+    theme: '',
+    sender: '',
+    receiver: '',
+    name: '',
+    category: 'general',
+    type: 'debit',
+  };
+
+
+  public startTransaction(transactionObject: any) {}
 }
