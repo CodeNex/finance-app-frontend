e8e182cacf8e7d1f0f38cbad4c06751b11197ae9
Richard Streu <rstreu13@icloud.com>
2025-03-29 16:34:27 +0100

    refactor: update transaction date handling and improve rendering logic in transactions summary

--- src/app/pages/home/content/overview/transactions-summary/transactions-summary.component.ts
+++ src/app/pages/home/content/overview/transactions-summary/transactions-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, Input, OnInit } from '@angular/core';
+import { Component, effect, inject} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IconsComponent } from '../../../../../components/icons/icons.component';
 import { DataStoreServiceService } from '../../../../../services/data-store-service.service';
@@ -9,25 +9,44 @@ import { SingleTransactionComponent } from './single-transaction/single-transact
 
 @Component({
   selector: 'app-transactions-summary',
-  imports: [RouterModule, IconsComponent, CommonModule, SingleTransactionComponent],
+  imports: [
+    RouterModule,
+    IconsComponent,
+    CommonModule,
+    SingleTransactionComponent,
+  ],
   templateUrl: './transactions-summary.component.html',
-  styleUrl: './transactions-summary.component.scss'
+  styleUrl: './transactions-summary.component.scss',
 })
-export class TransactionsSummaryComponent implements OnInit {
+export class TransactionsSummaryComponent {
   public dataStore: DataStoreServiceService = inject(DataStoreServiceService);
   public authService: AuthenticationService = inject(AuthenticationService);
   public apiService: APIService = inject(APIService);
 
-  public transactionsArray: any = this.dataStore.transactions;
+  public transactionsArraySignal$: any = this.dataStore.transactions;
+
+  public readyToRenderArray: any[] = [];
 
   constructor() {
     effect(() => {
-      let potsSignal = this.transactionsArray();
+      let potsSignal = this.transactionsArraySignal$();
       this.ngOnInit();
+     
     });
   }
+
   ngOnInit() {
-    // console.log(this.transactionsArray());
-    
+    this.readyToRenderArray = this.sortByDate(this.transactionsArraySignal$());
+  }
+
+
+  private sortByDate(array: any) {
+    return array.sort((a: any, b: any) => {
+      if (!a.execute_on) return 1;
+      if (!b.execute_on) return -1;
+      return (
+        new Date(b.execute_on).getTime() - new Date(a.execute_on).getTime()
+      );
+    });
   }
 }
