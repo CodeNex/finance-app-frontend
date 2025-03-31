65fb10baafd745c5bf2e20712711c15f722137bc
Richard Streu <rstreu13@icloud.com>
2025-03-31 12:52:21 +0200

    feat: add recurring delete modal component with initial template and test setup

---
+++ src/app/pages/home/content/recurring-bills/recurring-modals/recurring-delete-modal/recurring-delete-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recurring-delete-modal',
+  imports: [],
+  templateUrl: './recurring-delete-modal.component.html',
+  styleUrl: './recurring-delete-modal.component.scss'
+})
+export class RecurringDeleteModalComponent {
+
+}
