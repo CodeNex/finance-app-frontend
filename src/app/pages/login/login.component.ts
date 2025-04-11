198ca550e614cbfb92580345e44026fd5706f4d9
Richard Streu <rstreu13@icloud.com>
2025-04-11 08:00:52 +0200

    refactor: update activeForm variable usage in login component for consistency and clarity

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { LoginFormComponent } from '@login/login-form/login-form.component';
+import { SignUpFormComponent } from '@login/sign-up-form/sign-up-form.component';
 import { LoadingScreenComponent } from '@components/loading-screen/loading-screen.component';
 import { WarningScreenComponent } from '@components/warning-screen/warning-screen.component';
 import { ImprintComponent } from '@imprint/imprint.component';
@@ -22,45 +23,57 @@ import { AuthenticationService } from '@services/authentication.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  public authService: AuthenticationService = inject(AuthenticationService);
+export class LoginComponent implements OnInit, OnDestroy {
+  public authService = inject(AuthenticationService);
 
-  public currentShownLoginWindow: string = 'loginForm';
-  // loginForm, signUpForm
+  // possible values: loginForm, signUpForm
+  public activeForm: string = 'loginForm';
 
+  private subscriptions = new Subscription();
+
+  ngOnInit(): void {
+    this.addSubscriptions();
+  }
+
+  /**
+   * Subscribes to observables from the AuthService to update the UI of loading and warning screens.
+   */
   public isLoadingScreenVisible: boolean = false;
-
   public isWarningScreenVisible: boolean = false;
 
+  private addSubscriptions() {
+    this.subscriptions.add(
+      this.authService.isloadingScreenVisible$.subscribe((value) => {
+        this.isLoadingScreenVisible = value;
+      })
+    );
+    this.subscriptions.add(
+      this.authService.isWarningScreenVisible$.subscribe((value) => {
+        this.isWarningScreenVisible = value;
+      })
+    );
+  }
+
+  /**
+   * Update UI screens based on auth state
+   */
   public isImprintComponentVisible: boolean = false;
 
-  /**
-   * Subscribes to observables from the AuthService to update the visibility of loading and warning screens.
-   * @memberof LoginComponent
-   */
-  ngOnInit() {
-    this.authService.isloadingScreenVisible$.subscribe((value) => {
-      this.isLoadingScreenVisible = value;
-    });
-    this.authService.isWarningScreenVisible$.subscribe((value) => {
-      this.isWarningScreenVisible = value;
-    });
-  }
-
-  /**
-   * @param windowName function to switch between login and sign up forms
-   */
   chooseLoginWindow(windowName: string) {
-    this.currentShownLoginWindow = windowName;
+    this.activeForm = windowName;
   }
 
-  switchToLogInComponent(currentShownLoginWindow: string) {
-    this.currentShownLoginWindow = currentShownLoginWindow;
+  switchToLogInComponent(activeForm: string) {
+    this.activeForm = activeForm;
     this.isImprintComponentVisible = false;
   }
 
   switchToImprintComponent($event: string) {
     this.isImprintComponentVisible = true;
-    this.currentShownLoginWindow = $event;
+    this.activeForm = $event;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
