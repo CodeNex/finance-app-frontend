c4ce20cfa90f102fc8bf4e24f81ec00baceda6cf
Richard Streu <rstreu13@icloud.com>
2025-04-13 21:00:01 +0200

    refactor: enhance ClickOutsideCloseDirective with lifecycle hooks and event handling

--- src/app/shared/directives/click-outside-close.directive.ts
+++ src/app/shared/directives/click-outside-close.directive.ts
@@ -1,12 +1,35 @@
-import { Directive, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Output,
+  EventEmitter,
+  Renderer2,
+  OnDestroy,
+  AfterViewInit,
+  inject,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appClickOutsideClose]'
+  selector: '[appClickOutsideClose]',
 })
-export class ClickOutsideCloseDirective {
+export class ClickOutsideCloseDirective implements AfterViewInit, OnDestroy {
 
-  constructor() {
-    renderer = new Renderer2();
-   }
+  @Output() clickOutside = new EventEmitter<boolean>();
 
+  private renderer = inject(Renderer2);
+  private elementRef = inject(ElementRef);
+  private unsubscribeClickOutside: () => void = () => null;
+
+  ngAfterViewInit(): void {
+    this.unsubscribeClickOutside = this.renderer.listen('document', 'click', (event) => {
+      const clickedInside = this.elementRef.nativeElement.contains(event.target);
+      if (!clickedInside) {
+        this.clickOutside.emit(true);
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeClickOutside?.();
+  }
 }
