81054eb460e3732c931971a00fa49a934f262c9e
Richard Streu <rstreu13@icloud.com>
2025-04-14 17:19:26 +0200

    refactor: update budgets handling to use Signal and improve template bindings in SpendingSummaryComponent

--- src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary.component.ts
+++ src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary.component.ts
@@ -1,9 +1,15 @@
-import { Component, effect, inject, Input, Signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  Signal,
+  computed
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SpendingSummaryItemComponent } from '@content/budgets/budget-overview/spending-summary/spending-summary-item/spending-summary-item.component';
-
 import { DataStoreServiceService } from '@services/data-store-service.service';
+
+import { SpendingSummaryItemComponent } from '@content/budgets/budget-overview/spending-summary/spending-summary-item/spending-summary-item.component';
 
 @Component({
   selector: 'app-spending-summary',
@@ -15,22 +21,10 @@ export class SpendingSummaryComponent {
   // #region Component Setup (DI, Outputs, Template Refs, Subscription)
   public dataStore = inject(DataStoreServiceService);
 
-  public budgetsArraySignal$: Signal<BudgetsObject[]> = this.dataStore.budgets;
- 
-  public budgetsArray: BudgetsObject[] = [];
+  public budgetsArraySignal: Signal<BudgetsObject[]> = this.dataStore.budgets;
 
   @Input() public inWhichSection: string = '';
   // #endregion
-   
-  constructor() {
-    effect(() => {
-      let signal = this.budgetsArraySignal$();
-      this.budgetsArray = this.budgetsArraySignal$().filter((element: BudgetsObject) => !element.deleted_at);
-    })
-  }
 
-  ngOnInit() {
-    this.budgetsArray = this.budgetsArraySignal$().filter((element: BudgetsObject) => !element.deleted_at);
-  }
-
+  public budgetsArray = computed(() => this.budgetsArraySignal().filter(b => !b.deleted_at));
 }
