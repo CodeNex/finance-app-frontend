4226c541d5aa6fc1c91aed9c152c93ecfeeace2c
Richard Streu <rstreu13@icloud.com>
2025-04-14 13:54:59 +0200

    refactor: update budget deletion confirmation to use modalObject name in DeleteBudgetModalComponent

--- src/app/pages/home/content/budgets/budgets-modals/delete-budget-modal/delete-budget-modal.component.ts
+++ src/app/pages/home/content/budgets/budgets-modals/delete-budget-modal/delete-budget-modal.component.ts
@@ -1,9 +1,16 @@
 import { Component, inject, Input } from '@angular/core';
 
 import { MainModalService } from '@services/main-modal.service';
-import { ApiBudgetsService } from '../../api-budgets.service';
+import { ApiBudgetsService } from '@content/budgets/api-budgets.service';
 import { CommonModule } from '@angular/common';
 
+/**
+ * * * DeleteBudgetModalComponent
+ * This component is responsible for displaying the delete budget modal.
+ * It shows the budget name and asks the user to confirm the deletion.
+ * It uses the MainModalService to manage the modal state and the ApiBudgetsService to delete the budget.
+ * It also handles the logic for closing the modal and deleting the budget.
+ */
 @Component({
   selector: 'app-delete-budget-modal',
   imports: [CommonModule],
@@ -11,27 +18,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './delete-budget-modal.component.scss',
 })
 export class DeleteBudgetModalComponent {
-  public mainModalService: MainModalService = inject(MainModalService);
-  public apiBudgetsService: ApiBudgetsService = inject(ApiBudgetsService);
-
-  // closes main modal and its children
-  public closeMainModal() {
-    this.mainModalService.hideMainModal();
-  }
-
-  public currentBudgetToDelete: string = 'CurrentBudget';
+  // #region Component Setup (DI, Outputs, Template Refs, Subscription)
+  public mainModalService = inject(MainModalService);
+  public apiBudgetsService = inject(ApiBudgetsService);
 
   @Input() public modalObject: any = {};
   @Input() public budgetIndex: number = 1;
-
-  public currentBudgetIndex: number = -1;
-
-  ngOnInit() {
-    this.currentBudgetIndex = this.budgetIndex;
-    if (this.modalObject.name) {this.currentBudgetToDelete = this.modalObject.name} ;
-  }
+  // #endregion
 
   deleteCurrentBudget() {
-    this.apiBudgetsService.deleteBudget(this.modalObject, this.currentBudgetIndex);
+    this.apiBudgetsService.deleteBudget(this.modalObject, this.budgetIndex);
+  }
+
+  public closeMainModal() {
+    this.mainModalService.hideMainModal();
   }
 }
