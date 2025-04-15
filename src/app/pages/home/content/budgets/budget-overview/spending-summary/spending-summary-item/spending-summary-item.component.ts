91f3186e9d1a0ed8180101bd3733695bcf5859fd
Richard Streu <rstreu13@icloud.com>
2025-04-15 11:30:09 +0200

    refactor: implement OnInit lifecycle hook and initialize budget calculations in SpendingSummaryItemComponent

--- src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary-item/spending-summary-item.component.ts
+++ src/app/pages/home/content/budgets/budget-overview/spending-summary/spending-summary-item/spending-summary-item.component.ts
@@ -4,10 +4,12 @@ import {
   inject,
   Input,
   Signal,
+  OnInit
 } from '@angular/core';
 import { FormatAmountPipe } from '@shared/pipes/format-amount.pipe';
 
 import { DataStoreServiceService } from '@services/data-store-service.service';
+import { BudgetCalculationsService } from '@services/budget-calculations.service';
 
 @Component({
   selector: 'app-spending-summary-item',
@@ -15,14 +17,28 @@ import { DataStoreServiceService } from '@services/data-store-service.service';
   templateUrl: './spending-summary-item.component.html',
   styleUrl: './spending-summary-item.component.scss',
 })
-export class SpendingSummaryItemComponent {
+export class SpendingSummaryItemComponent implements OnInit {
   // #region Component Setup (DI, Outputs, Template Refs, Subscription)
   private dataStore = inject(DataStoreServiceService);
+  private budgetCalculationsService = inject(BudgetCalculationsService);
 
   public budgetsArraySignal: Signal<BudgetsObject[]> = this.dataStore.budgets;
 
   @Input() public summaryItem!: BudgetsObject;
 
   @Input() public inWhichSection: string = '';
+
+  public budgetCalculations: BudgetCalculations = {
+    calculatedSpent: 0,
+    remaining: 0,
+    isTooMuchSpent: false,
+  };
   // #endregion
+
+  // #region Lifecycle Hooks
+  ngOnInit(): void {
+    this.budgetCalculations = this.budgetCalculationsService.calculateBudget(this.summaryItem, 'year');
+  }
+  // #endregion
+
 }
