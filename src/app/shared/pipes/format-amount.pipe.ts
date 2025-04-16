06b7339bc3144e0172f8e062255d4cb8312b0833
Richard Streu <rstreu13@icloud.com>
2025-04-16 10:01:37 +0200

    refactor: enhance documentation for FormatAmountPipe and FormatDatePipe for clarity and maintainability

--- src/app/shared/pipes/format-amount.pipe.ts
+++ src/app/shared/pipes/format-amount.pipe.ts
@@ -1,5 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * * FormatAmountPipe
+ * * This pipe is responsible for formatting a number into a currency string.
+ * * * It takes a number as input and returns a formatted string with a dollar sign and two decimal places.
+ * * * It also takes an optional parameter to specify whether the amount is a credit or debit.
+ */
 @Pipe({
   name: 'formatAmount',
 })
--- src/app/shared/pipes/format-date.pipe.ts
+++ src/app/shared/pipes/format-date.pipe.ts
@@ -1,11 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * * FormatDatePipe
+ * This pipe is responsible for formatting a date string into a more readable format.
+ * It takes a date string as input and returns a formatted date string.
+ * It uses the toLocaleDateString method to format the date.
+ */
 @Pipe({
   name: 'formatDate'
 })
 export class FormatDatePipe implements PipeTransform {
 
-  transform(value: unknown,): unknown {
+  transform(value: string,): string {
     if (!value) {
       console.log('No value provided for date formatting');
       return '';
