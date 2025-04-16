4699e9ee9bf371bc7d6862a6e289008b9fcc3e13
Richard Streu <rstreu13@icloud.com>
2025-04-16 12:30:58 +0200

    refactor: update pots component to use Signal and computed for potsArray

--- src/app/pages/home/content/pots/pots.component.ts
+++ src/app/pages/home/content/pots/pots.component.ts
@@ -1,25 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DataStoreServiceService } from '@services/data-store-service.service';
 import { AuthenticationService } from '@services/authentication.service';
 import { MainModalService } from '@services/main-modal.service';
-import { SinglePotComponent } from './single-pot/single-pot.component';
+import { SinglePotComponent } from '@content/pots/single-pot/single-pot.component';
 
+/**
+ * * * PotsComponent
+ * * This component is responsible for displaying the pots in the application.
+ */
 @Component({
   selector: 'app-pots',
-  imports: [ CommonModule, SinglePotComponent ],
+  imports: [CommonModule, SinglePotComponent],
   templateUrl: './pots.component.html',
   styleUrl: './pots.component.scss',
 })
 export class PotsComponent {
-  private dataStore: DataStoreServiceService = inject(DataStoreServiceService);
-  public authService: AuthenticationService = inject(AuthenticationService);
-  public mainModalService: MainModalService = inject(MainModalService);
+  // #region Component Setup (DI, Outputs, Template Refs, Subscription)
+  private dataStore = inject(DataStoreServiceService);
+  public authService = inject(AuthenticationService);
+  public mainModalService = inject(MainModalService);
 
-  public potsArray: any = this.dataStore.pots; 
+  public potsSignal: Signal<PotsObject[]> = this.dataStore.pots;
 
-  public openSubModal(subModal: string, subModalObject: Object) {
+  public readonly potsArray = computed(() => this.potsSignal())();
+  // #endregion
+
+  /**
+   * @description - This function is responsible for opening the main modal in the application.
+   * @param subModal - The name of the sub modal to be opened.
+   * @param subModalObject - The object that contains the data for the sub modal.
+   */
+  public openSubModal(subModal: string, subModalObject: Object): void {
     this.mainModalService.chooseSubModal(subModal, subModalObject, null);
   }
 }
