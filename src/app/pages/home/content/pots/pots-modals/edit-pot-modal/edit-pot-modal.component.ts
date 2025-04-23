4933be73576ba891a92d82b77b667f6c42539bed
Richard Streu <rstreu13@icloud.com>
2025-04-23 09:54:56 +0200

    refactor: simplify pot target input handling and integrate formatting service in EditPotModalComponent

--- src/app/pages/home/content/pots/pots-modals/edit-pot-modal/edit-pot-modal.component.ts
+++ src/app/pages/home/content/pots/pots-modals/edit-pot-modal/edit-pot-modal.component.ts
@@ -40,10 +40,6 @@ export class EditPotModalComponent {
   public currentPotIndex: number = -1;
   // #endregion
 
-  
-  public potTargetInputValue: string = '0.00';
-  public potTargetString: string = '0.00';
-
   // #region Lifecycle Hooks
   ngOnInit(): void {
     this.initializePotAndModal();
@@ -58,7 +54,6 @@ export class EditPotModalComponent {
     this.potTargetInputValue = this.currentPot.target.toLocaleString('en-US', {
       minimumFractionDigits: 2,
     });
-    this.potTargetString = this.potTargetInputValue;
   }
   // #endregion
 
@@ -101,71 +96,19 @@ export class EditPotModalComponent {
     }
   }
 
-  // controls the maximum amount of the pot target
-  controlMaxTarget(event: any) {
-    const deleteKeys = ['Backspace', 'Delete'];
-    const otherKeys = ['ArrowLeft', 'ArrowRight', 'Tab'];
-    const isNumberKey = /^[0-9]$/.test(event.key);
+  // the value of the pot target input binded by ngModel
+  public potTargetInputValue: string = '0.00';
 
-    if (isNumberKey) {
-      event.preventDefault();
-      this.addNumberToTargetInput(event);
-    } else if (deleteKeys.includes(event.key)) {
-      event.preventDefault();
-      this.deleteNumberFromTargetInput();
-    } else if (otherKeys.includes(event.key)) {
-      return;
-    } else {
-      event.preventDefault();
-      return;
-    }
-  }
-
-  // add a number to the target input
-  addNumberToTargetInput(event: any) {
-    let currentTarget = this.potTargetString;
-    let numbersArray = currentTarget.replace(/[.,]/g, '').split('');
-    if (numbersArray.length === 3 && numbersArray[0] === '0') {
-      numbersArray.shift();
-      numbersArray.push(event.key);
-      numbersArray.splice(numbersArray.length - 2, 0, '.');
-      this.potTargetString = parseFloat(numbersArray.join('')).toLocaleString(
-        'en-US',
-        {
-          minimumFractionDigits: 2,
-        }
-      );
-      this.potTargetInputValue = this.potTargetString;
-    } else if (
-      numbersArray.length >= 3 &&
-      numbersArray.length < 11 &&
-      numbersArray[0] !== '0'
-    ) {
-      numbersArray.push(event.key);
-      numbersArray.splice(numbersArray.length - 2, 0, '.');
-      this.potTargetString = parseFloat(numbersArray.join('')).toLocaleString(
-        'en-US',
-        {
-          minimumFractionDigits: 2,
-        }
-      );
-      this.potTargetInputValue = this.potTargetString;
-    }
-  }
-
-  // delete a number from the target input
-  deleteNumberFromTargetInput() {
-    let currentTarget = this.potTargetString;
-    let numbersArray = currentTarget.replace(/[.,]/g, '').split('');
-    numbersArray.pop();
-    numbersArray.splice(numbersArray.length - 2, 0, '.');
-    this.potTargetString = parseFloat(numbersArray.join('')).toLocaleString(
-      'en-US',
-      {
-        minimumFractionDigits: 2,
-      }
+  /**
+   * @description - This function is used to format the amount input value.
+   * @param event - The event that is triggered when the user types in the input field.
+   */
+  public controlMaxTarget(event: KeyboardEvent): void {
+    const inputValue = this.potTargetInputValue;
+    this.potTargetInputValue = this.formatAmountInputService.formatAmountInput(
+      event,
+      inputValue
     );
-    this.potTargetInputValue = this.potTargetString;
   }
   // #endregion
 
