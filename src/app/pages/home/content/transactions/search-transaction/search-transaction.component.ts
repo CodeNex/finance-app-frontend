7c3feade7ca1c87989a73ce5439a3f098d01f419
Richard Streu <rstreu13@icloud.com>
2025-04-25 14:54:07 +0200

    refactor: update SearchTransactionComponent to use template reference variable for input field and enhance documentation for clarity

--- src/app/pages/home/content/transactions/search-transaction/search-transaction.component.ts
+++ src/app/pages/home/content/transactions/search-transaction/search-transaction.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { IconsComponent } from '@components/icons/icons.component';
 
+/**
+ * * * SearchTransactionComponent
+ * This component is responsible for displaying the search input field for transactions.
+ * It allows the user to search for transactions by entering a search term.
+ * It uses the CommonModule for common Angular directives and the IconsComponent for displaying icons.
+ */
 @Component({
   selector: 'app-search-transaction',
   imports: [CommonModule, IconsComponent],
@@ -9,20 +15,18 @@ import { IconsComponent } from '@components/icons/icons.component';
   styleUrl: './search-transaction.component.scss'
 })
 export class SearchTransactionComponent {
-
+  // #region Component Setup (DI, Outputs, Template Refs, Subscription)
   @Output() public searchFieldChange: EventEmitter<string> = new EventEmitter<string>();
+
+  @ViewChild('transactionSearchInputField') public transactionSearchInputField!: ElementRef<HTMLInputElement>;
 
   public emitSearchFieldChange(input: string) {
     this.searchFieldChange.emit(input);
   }
+  // #endregion
 
-  ngOnInit() {
-      this.emitSearchFieldChange(''); 
-  }
-
-  public searchFieldInput() {
-    let field = document.getElementById('transactionSearchInputField') as HTMLInputElement;
-    let value = field.value;
+  public searchFieldInput(): void {
+    let value = this.transactionSearchInputField.nativeElement.value;
     this.emitSearchFieldChange(value);
   }
 }
