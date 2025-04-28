0162c12c5baff2f5fa7b188c3dfa429637a6e0c2
Richard Streu <rstreu13@icloud.com>
2025-04-28 15:24:07 +0200

    refactor: update PotsSummaryComponent to use Signal for reactive data handling; simplify total savings calculation and remove unused properties

--- src/app/pages/home/content/overview/pots-summary/pots-summary.component.ts
+++ src/app/pages/home/content/overview/pots-summary/pots-summary.component.ts
@@ -1,44 +1,35 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, Signal } from '@angular/core';
 import { IconsComponent } from '@components/icons/icons.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DataStoreServiceService } from '@services/data-store-service.service';
-import { AuthenticationService } from '@services/authentication.service';
-import { APIService } from '@services/api.service';
 
+/**
+ * * * PotsSummaryComponent
+ * This component is responsible for displaying the summary of pots in the application.
+ * It uses the DataStoreService to get the pots data and calculates the total savings.
+ * It uses the effect function to reactively update the total savings when the pots data changes.
+ */
 @Component({
   selector: 'app-pots-summary',
   imports: [IconsComponent, RouterModule, CommonModule],
   templateUrl: './pots-summary.component.html',
-  styleUrl: './pots-summary.component.scss'
+  styleUrl: './pots-summary.component.scss',
 })
-export class PotsSummaryComponent implements OnInit {
-  public dataStore: DataStoreServiceService = inject(DataStoreServiceService);
-  public authService: AuthenticationService = inject(AuthenticationService);
-  public apiService: APIService = inject(APIService);
+export class PotsSummaryComponent {
+  // #region Component Setup (DI, Outputs, Template Refs, Subscription)
+  public dataStore = inject(DataStoreServiceService);
 
-  public potsArray: any = this.dataStore.pots;
+  public potsArraySignal: Signal<PotsObject[]> = this.dataStore.pots;
+
+  public potsSummaryEffect = effect(() => {
+    this.totalSavings = this.getTotalSavedAmount(this.potsArraySignal());
+  });
 
   public totalSavings: number = 0;
+  // #endregion
 
-  public formattedTotalSavings: string = '';
-
-  constructor() {
-    effect(() => {
-      let potsSignal = this.potsArray();
-      this.ngOnInit();
-    });
-  }
-
-  ngOnInit() {
-    this.updateTotalSavings();
-  }
-
-  updateTotalSavings() {
-    this.totalSavings = this.getTotalSavedAmount(this.potsArray());
-    this.formattedTotalSavings = this.getformattedValue(this.totalSavings);
-  }
-
+  // #region Helper Functions
   getTotalSavedAmount(potsArray: PotsObject[]): number {
     let totalSavings = 0;
     for (let i = 0; i < potsArray.length; i++) {
@@ -46,12 +37,5 @@ export class PotsSummaryComponent implements OnInit {
     }
     return totalSavings;
   }
-
-  getformattedValue(value: number): string {
-    return value.toLocaleString('en-US', {
-      maximumFractionDigits: 0,
-    });
-  }
+  // #endregion
 }
-
-
