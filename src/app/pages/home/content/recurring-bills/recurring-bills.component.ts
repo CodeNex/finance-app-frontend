b7d858f89b4767c178bd28099542f06477ef4769
Richard Streu <rstreu13@icloud.com>
2025-04-28 10:15:31 +0200

    refactor: update import paths and streamline dependency injection in RecurringBillsComponent

--- src/app/pages/home/content/recurring-bills/recurring-bills.component.ts
+++ src/app/pages/home/content/recurring-bills/recurring-bills.component.ts
@@ -2,8 +2,8 @@ import { Component, effect, inject } from '@angular/core';
 import { DataStoreServiceService } from '@services/data-store-service.service';
 import { CommonModule } from '@angular/common';
 import { AuthenticationService } from '@services/authentication.service';
-import { BillsSummaryComponent } from './bills-summary/bills-summary.component';
-import { RecurringBillsListComponent } from './recurring-bills-list/recurring-bills-list.component';
+import { BillsSummaryComponent } from '@content/recurring-bills/bills-summary/bills-summary.component';
+import { RecurringBillsListComponent } from '@content/recurring-bills/recurring-bills-list/recurring-bills-list.component';
 
 @Component({
   selector: 'app-recurring-bills',
@@ -12,17 +12,16 @@ import { RecurringBillsListComponent } from './recurring-bills-list/recurring-bi
   styleUrl: './recurring-bills.component.scss'
 })
 export class RecurringBillsComponent {
-
-  private dataStore: DataStoreServiceService = inject(DataStoreServiceService);
-  public authService: AuthenticationService = inject(AuthenticationService);
+  // #region Component Setup (DI, Outputs, Template Refs, Subscription)
+  private dataStore = inject(DataStoreServiceService);
+  public authService = inject(AuthenticationService);
 
   public recurringBillsArray = this.dataStore.transactionsRecurring();
-  public transactionsArray$ = this.dataStore.transactions();
+  public transactionsArray = this.dataStore.transactions();
   
-  constructor() {
-    effect(() => {
-      this.recurringBillsArray = this.dataStore.transactionsRecurring();
-      this.transactionsArray$ = this.dataStore.transactions();
-    });
-  }
+  public recurringBillsEffect = effect(() => {
+    this.recurringBillsArray = this.dataStore.transactionsRecurring();
+    this.transactionsArray = this.dataStore.transactions();
+  })
+  // #endregion
 }
