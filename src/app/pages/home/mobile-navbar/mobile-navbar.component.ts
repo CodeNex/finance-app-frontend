8c3c69d2fdec837da6eae4cd46c6e14e9da48bdd
Richard Streu <rstreu13@icloud.com>
2025-04-29 12:26:46 +0200

    refactor: implement MobileNavbar component with navigation structure and authentication integration

---
+++ src/app/pages/home/mobile-navbar/mobile-navbar.component.ts
@@ -0,0 +1,23 @@
+import {
+  Component,
+  inject,
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IconsComponent } from '@components/icons/icons.component';
+
+import { AuthenticationService } from '@services/authentication.service';
+import { BasedataService } from '@services/basedata.service';
+
+@Component({
+  selector: 'app-mobile-navbar',
+  imports: [RouterModule, IconsComponent],
+  templateUrl: './mobile-navbar.component.html',
+  styleUrl: './mobile-navbar.component.scss'
+})
+export class MobileNavbarComponent {
+  public authService = inject(AuthenticationService);
+  private baseData = inject(BasedataService);
+
+  public navData = this.baseData.navdata;
+
+}
