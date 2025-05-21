ed03b43c28d0eca501cfe25cb689f972a9098fc6
Richard Streu <rstreu13@icloud.com>
2025-05-21 17:19:42 +0200

    refactor: add CommonModule import to AverageRecurringComponent and adjust layout in bills summary

--- src/app/pages/home/content/recurring-bills/average-recurring/average-recurring.component.ts
+++ src/app/pages/home/content/recurring-bills/average-recurring/average-recurring.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-average-recurring',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './average-recurring.component.html',
   styleUrl: './average-recurring.component.scss'
 })
