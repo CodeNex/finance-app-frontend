d1bdd1bf837d7f44af2f94023f13f8891421b14f
Richard Streu <rstreu13@icloud.com>
2025-05-21 18:00:45 +0200

    refactor: enhance dropdown functionality in bills summary component

--- src/app/pages/home/content/recurring-bills/average-recurring/average-recurring.component.ts
+++ src/app/pages/home/content/recurring-bills/average-recurring/average-recurring.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './average-recurring.component.scss'
 })
 export class AverageRecurringComponent {
+// #region Component Setup (DI, Outputs, Template Refs, Subscription)
+  public selectedTimeWindow: string = 'monthly';
+  public selectedTimeWindowName: string = 'This Month';
+
+  public timeFrames: TimeFrame[] = [
+    { name: 'per Month', value: 'monthly' },
+    { name: 'per Quarter', value: 'quarterly' },
+    { name: 'per Year', value: 'yearly' },
+  ];
+// #endregion
+
+// #region Helper Functions
+  public isDropDownOpen: boolean = false;
+
+  public toggleDropDown(): void {
+    this.isDropDownOpen = !this.isDropDownOpen;
+  }
+
+  public updateTimeWindow(timeFrame: TimeFrame) {
+    this.selectedTimeWindow = timeFrame.value;
+    this.selectedTimeWindowName = timeFrame.name;
+  }
+// #endregion
 
 }
