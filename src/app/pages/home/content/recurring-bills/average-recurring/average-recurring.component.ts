6137db65796d4173ab6283ea303ba2853bb246cc
Richard Streu <rstreu13@icloud.com>
2025-05-21 17:00:43 +0200

    feat: add AverageRecurringComponent with initial template and test setup

---
+++ src/app/pages/home/content/recurring-bills/average-recurring/average-recurring.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-average-recurring',
+  imports: [],
+  templateUrl: './average-recurring.component.html',
+  styleUrl: './average-recurring.component.scss'
+})
+export class AverageRecurringComponent {
+
+}
