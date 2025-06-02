6b72fad4cbc3384f4511833cb061e6f4721a0b83
Richard Streu <rstreu13@icloud.com>
2025-06-02 09:37:10 +0200

    fix: update breakpoint values in ScreensizeService and add smallTablet breakpoint

--- src/app/services/screensize.service.ts
+++ src/app/services/screensize.service.ts
@@ -3,20 +3,31 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+/**
+ * * * ScreensizeService
+ * This service is responsible for observing the screen size and providing  
+ * observables for different screen size breakpoints.
+ * It uses Angular's BreakpointObserver to monitor changes in screen size.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class ScreensizeService {
   private customBreakpoints = {
     handset: '(max-width: 768px)',
+    smallTablet: '(min-width: 769px) and (max-width: 1280px)',
     tablet: '(max-width: 1280px)',
     desktop: '(min-width: 1281px)',
-  }
+  };
 
   private breakpointObserver = inject(BreakpointObserver);
 
   public isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(this.customBreakpoints.handset)
+    .pipe(map((result) => result.matches));
+
+  public isSmallTablet$: Observable<boolean> = this.breakpointObserver
+    .observe(this.customBreakpoints.smallTablet)
     .pipe(map((result) => result.matches));
 
   public isTablet$: Observable<boolean> = this.breakpointObserver
