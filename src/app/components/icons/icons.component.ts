5385babed44ca16cc0d078858f584e45c997258e
Richard Streu <rstreu13@icloud.com>
2025-06-03 08:51:02 +0200

    fix: update icon name in search transaction component to 'searchIcon' for consistency

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * * IconsComponent
@@ -9,16 +9,53 @@ import { Component, Input, } from '@angular/core';
   selector: 'app-icons',
   imports: [],
   templateUrl: './icons.component.html',
-  styleUrl: './icons.component.scss'
+  styleUrl: './icons.component.scss',
 })
 export class IconsComponent {
-  @Input("iconName") public iconName: string = "";
+  @Input('iconName') public iconName: string = '';
 
-  private iconNames: string[] = ['overview', 'transactions', 'budgets', 'pots', 'potsTransparentGreen', 'recurringBills', 'settings', 'imprint', 'logout', 'slideButton', 'openedEye', 'closedEye', 'checked', 'backArrowWhite', 'dotsThree', 'dropOpen', 'dropClose', 'themeCheck', 'entertainment', 'bills', 'groceries', 'diningOut', 'transportation', 'personalCare', 'education', 'lifestyle', 'shopping', 'general', 'billsIcon', 'searchLensIcon', 'smallArrowRight', 'searchIcon', 'trashCan'];
+  private iconNames: string[] = [
+    'overview',
+    'transactions',
+    'budgets',
+    'pots',
+    'potsTransparentGreen',
+    'recurringBills',
+    'settings',
+    'imprint',
+    'logout',
+    'slideButton',
+    'openedEye',
+    'closedEye',
+    'checked',
+    'backArrowWhite',
+    'dotsThree',
+    'dropOpen',
+    'dropClose',
+    'themeCheck',
+    'entertainment',
+    'bills',
+    'groceries',
+    'diningOut',
+    'transportation',
+    'personalCare',
+    'education',
+    'lifestyle',
+    'shopping',
+    'general',
+    'billsIcon',
+    'searchLensIcon',
+    'smallArrowRight',
+    'searchIcon',
+    'trashCan',
+  ];
 
   ngOnInit(): void {
     if (!this.iconNames.includes(this.iconName)) {
-      console.warn(`Icon name "${this.iconName}" is not recognized. Please check the icon name.`);
-      this.iconName = 'noAviableIcon'; 
-  }}
+      console.warn(
+        `Icon name "${this.iconName}" is not recognized. Please check the icon name.`
+      );
+      this.iconName = 'noAviableIcon';
+    }
+  }
 }
